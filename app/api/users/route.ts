92e5d879334f1cd2b04082cbe3b0716bbc06faa8
seedevk8s <seedevk8s@gmail.com>
2025-05-15 10:49:28 +0900

    유저를 DB에서 검색하는 db 샘플 준비

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
 
+const DB = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Doe' },
+    { id: 3, name: 'John Smith' },
+]
+
 export async function GET(request: Request) {
     return NextResponse.json({
-        users: [
-            { id: 1, name: 'John Doe' },
-            { id: 2, name: 'Jane Doe' },
-            { id: 3, name: 'John Smith' },
-        ]
+        users: DB
     })
 }
