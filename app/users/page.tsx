fa1d6c8bcf2f2b4a556c5cf28aa2bc4065d8248f
seedevk8s <seedevk8s@gmail.com>
2025-05-15 20:22:42 +0900

    DB 배열에 있는 사용자 중 name이 HoJin을 포함하는 항목을 필터링

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,15 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function UsersPage() {
     const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(`/api/users?name=${"HoJin"}`)
+            .then((res) => res.json())
+            .then((data) => setUsers(data.users));
+    }, []);
 
     return (
         <main>
