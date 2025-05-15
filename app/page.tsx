53b455d4982915982851322fbe0dfa49427ea558
seedevk8s <seedevk8s@gmail.com>
2025-05-15 21:31:35 +0900

    메타데이터를 어플리케이션에 추가- Static Metadata 방식

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: 'Next.js Tutorial',
+  description: 'Next.js Tutorial 배우기',
+};
 
 export default function Home() {
   return (
     <main>
       Home
-      <Link href="/dashboard">Go To Dashboard</Link>
+      <Link href="/dashboard"> Go To Dashboard</Link>
     </main>
   );
 }
