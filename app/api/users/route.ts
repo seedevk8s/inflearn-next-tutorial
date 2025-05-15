6918be8b98ac2da9266f8f7118cb59a0b1866a40
seedevk8s <seedevk8s@gmail.com>
2025-05-15 12:04:55 +0900

    Users/Page 함수 컴포넌트가 클라이언트 측에서 렌더링

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,15 +4,17 @@ const DB = [
     { id: 1, name: 'HoJin Chu' },
     { id: 2, name: 'Jane Doe' },
     { id: 3, name: 'John Smith' },
-]
+];
 
 export async function GET(request: Request) {
     const searchParams = new URL(request.url).searchParams;
     const name = searchParams.get('name');
     if (name === null) {
-        return NextResponse.json({ users: [] })
-    
+        // 파라미터가 없을 경우 처리
+        return NextResponse.json({ users: [] });
+    }
+    // 파라미터가 있을 경우 처리
     return NextResponse.json({
         users: DB.filter((user) => user.name.includes(name))
-    })
+    });
 }
